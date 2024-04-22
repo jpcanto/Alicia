2bcdfbb58777c38ce92a6db43ecae4b08a192671
jpcanto <canto.jplopes@gmail.com>
2024-04-22 18:59:15 -0300

    feat: improve audio player method

--- alicia_class.py
+++ alicia_class.py
@@ -19,16 +19,21 @@ class Alicia:
         self.quit = False
         self.callback = []
 
+        self.create_environment()
         self.loop()
+
+    def create_environment():
+        audio_folder = "audios"
+
+        if not os.path.exists(audio_folder):
+            os.makedirs(audio_folder)
 
     def microphone_listen(self):
         microphone = sr.Recognizer()
         with sr.Microphone() as source:
             microphone.adjust_for_ambient_noise(source)
 
-            path = os.path.abspath(os.path.join("default_audios", "talk.mp3"))
-            formatted_path = path.replace(" ", "%20")
-            playsound(formatted_path)
+            self.play_audio("Pode falar", "default.mp3")
 
             try:
                 audio = microphone.listen(source, timeout=5)
@@ -44,34 +49,50 @@ class Alicia:
             self.microphone_error_handler()
 
     def microphone_error_handler(self):
-        path = os.path.abspath(os.path.join("default_audios", "error.mp3"))
-        formatted_path = path.replace(" ", "%20")
-        playsound(formatted_path)
+        self.play_audio("Não entendi", "error.mp3")
 
         time.sleep(3)
         return self.microphone_listen()
 
     def play_audio(self, txt, file="last_command.mp3"):
-        tts = gTTS(txt, lang="pt-BR")
-        time.sleep(0.5)
+        audios_path = "audios"
+        file_path = os.path.join(audios_path, file)
 
-        try:
-            tts.save(file)
-        except:
-            print("last_command file save error")
+        if not os.path.exists(file_path):
+            tts = gTTS(txt, lang="pt-BR")
+            time.sleep(0.5)
 
-        path = os.path.abspath(file)
-        formatted_path = path.replace(" ", "%20")
-        playsound(formatted_path)
+            try:
+                tts.save(file_path)
+            except:
+                print("last_command file save error")
+
+        playsound(file_path)
 
     def loop(self, txt=""):
         txt = txt + "" + self.microphone_listen()
         if any(word in txt for word in self.quit_keywords):
             self.quit = True
 
-        # TODO add orders
         elif any(word in txt for word in self.order_keywords):
-            pass
+            if not "path_configured" in txt:
+                self.play_audio("Qual pedido de sempre?", "alternative_command.mp3")
+                return self.loop(f"path_configured {txt}")
+
+            if any(word in txt for word in ["um", "1"]):
+                self.callback = ["default", 1]
+                print("order 1")
+
+            if any(word in txt for word in ["dois", "2"]):
+                self.callback = ["default", 2]
+                print("order 2")
+
+            if any(word in txt for word in ["três", "3"]):
+                self.callback = ["default", 3]
+                print("order 3")
+
+        else:
+            return self.microphone_listen()
 
         print(txt)
 
